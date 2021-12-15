7b8e2428878455b923cfdd08db16e46e4c9ec32f
Afiz <afizshaik@in.ibm.com>
2021-12-15 22:27:20 +0530

    added serializer method filed

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -4,13 +4,17 @@ from rest_framework import serializers
 from rest_framework.fields import ReadOnlyField
 
 class MovieSerializer(serializers.ModelSerializer):
-    
+    len_name = serializers.SerializerMethodField()
     class Meta:
         model = Movie
         fields = '__all__'
         # fields = ['id', 'name', 'description']
         # exclude = ['active']
     
+    def get_len_name(self, object):
+        return len(object.name)
+
+
     def validate_description(self, value):
         if len(value) < 2:
             raise serializers.ValidationError('Description is too short')
@@ -22,35 +26,3 @@ class MovieSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Name and description both are same.')
         else:
             return data
-
-
-# def name_length(value):
-#     if len(value) < 2:
-#         raise serializers.ValidationError('Name is too short')
-# class MovieSerializer(serializers.Serializer):
-#     id = serializers.IntegerField(read_only=True)
-#     name = serializers.CharField(validators=[name_length])
-#     description = serializers.CharField()
-#     active = serializers.BooleanField()
-
-#     def create(self, validated_data):
-#         return Movie.objects.create(**validated_data)
-    
-#     def update(self, instance, validated_data):
-#         instance.name = validated_data.get('name', instance.name)
-#         instance.description = validated_data.get('description', instance.description)
-#         instance.active = validated_data.get('active', instance.active)
-#         instance.save()
-#         return instance
-
-#     def validate_description(self, value):
-#         if len(value) < 2:
-#             raise serializers.ValidationError('Description is too short')
-#         else:
-#             return value
-    
-#     def validate(self, data):
-#         if data['name'] == data['description']:
-#             raise serializers.ValidationError('Name and description both are same.')
-#         else:
-#             return data
